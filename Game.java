827f0a8b739f1afb7205982458e6950826256d67
Sergey <sefimenko13@gmail.com>
2017-02-28 13:51:13 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,40 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-public class Game {
-	
-	private final int ONEDECK_SHIPS = 4;
-	private final int TWODECK_SHIPS = 3;
-	private final int THREEDECK_SHIPS = 2;
-	private final int FOURDECK_SHIPS = 1;
-	private final int ONE_DECK = 1;
-	private final int TWO_DECK = 2;
-	private final int THREE_DECK = 3;
-	private final int FOUR_DECK = 4;
-	private final int CLASSES = 4;
-	private final int SHIPS = 10;
-	private final int NUM_OF_PLAYERS = 2;
-	String[] heading = {" A", " B", " C", " D", " E", " F", " G", " H", " I", " J"};
-	String[][] field = new String[10][10];
-	ArrayList<Ship> ships = new ArrayList<Ship>();
-	ArrayList<Cells> occupied = new ArrayList<Cells>();
-	ArrayList<Player> players = new ArrayList<Player>();
-	
-	
-	
-	public void createField(){
-		for(int i=0; i<field.length; i++){
-			
-			for(int j=0; j<field[i].length; j++){
-				Cell c = new Cell("Empty");
-				field [i][j] = c.getShape();
-			}
-			
-		}
-	}
-	
-	/*public void drawBoard(){
-		System.out.print(" " + "\t");
+public class Display{
+String[] heading = {" A", " B", " C", " D", " E", " F", " G", " H", " I", " J"};
+public void salute(){
+System.out.println("Hello, sailor! What be yarrr name?");
+}
+ 
+ public String getPlayerName(){
+  Scanner s = new Scanner(System.in);
+  String playerName = s.nextLine();
+  return playerName;
+ }
+ 
+ public void printField(String[][] field){
+  System.out.print(" " + "\t");
 		for(int h=0; h<heading.length; h++){
 		System.out.print(heading[h]);
 		}
@@ -45,82 +25,17 @@ public class Game {
 				System.out.print(field[i][j]);
 			}
 			System.out.println("|");
-		}
-	}
-	*/
-	
-	public void fillField(){
-		
-		for(int j=0; j<SHIPS; j++){
-			for(int i=0; i<ONEDECK_SHIPS; i++){
-		Ship s = new Ship(ONE_DECK, new Random().nextInt(field.length), new Random().nextInt(field[1].length));
-		Cell c = new Cell("Deck");
-		field[s.decksList.get(0).getX()][s.decksList.get(0).getY()] = c.getShape();
-			ships.add(s);
-		}
-			for(int i=0; i<TWODECK_SHIPS; i++){
-		Ship s = new Ship(TWO_DECK, new Random().nextInt(field.length), new Random().nextInt(field[1].length));
-		Cell c = new Cell("Deck");
-		field[s.decksList.get(0).getX()][s.decksList.get(0).getY()] = c.getShape();
-			ships.add(s);
-		}
-			
-			for(int i=0; i<THREEDECK_SHIPS; i++){
-		Ship s = new Ship(THREE_DECK, new Random().nextInt(field.length), new Random().nextInt(field[1].length));
-		Cell c = new Cell("Deck");
-		field[s.decksList.get(0).getX()][s.decksList.get(0).getY()] = c.getShape();
-			ships.add(s);
-		}
-			
-			for(int i=0; i<FOURDECK_SHIPS; i++){
-		Ship s = new Ship(FOUR_DECK, new Random().nextInt(field.length), new Random().nextInt(field[1].length));
-		Cell c = new Cell("Deck");
-		field[s.decksList.get(0).getX()][s.decksList.get(0).getY()] = c.getShape();
-			ships.add(s);
-		}
-			
-		d.shipsAreReady();
-	}
-	
-	public void shot(){
-		System.out.println("Shoot!");
-		Scanner s = new Scanner(System.in);
-		String shot = s.nextLine();
-		shot = shot.replaceAll("[Aa]", "0");
-		shot = shot.replaceAll("[Bb]", "1");
-		shot = shot.replaceAll("[Cc]", "2");
-		shot = shot.replaceAll("[Dd]", "3");
-		shot = shot.replaceAll("[Ee]", "4");
-		shot = shot.replaceAll("[Ff]", "5");
-		shot = shot.replaceAll("[Gg]", "6");
-		shot = shot.replaceAll("[Hh]", "7");
-		shot = shot.replaceAll("[Ii]", "8");
-		shot = shot.replaceAll("[Jj]", "9");
-		//char[] sh = shot.toCharArraY();
-		Cell c = new Cell("Miss");
-		System.out.println(shot);
-	  field[(Character.getNumericValue(shot.charAt(1))+1)][Character.getNumericValue(shot.charAt(0))] = c.getShape();
-		
-	}
-		
-		public void endGame(){
-			d.weHaveWinner(players.get(0).getName());
-		}
-	
-	// Start
-	public static void main(String[] args){
-		Game g = new Game();
-		Display d = new Display();
-		d.salute;
-		Player playerOne = new Player();
-		playerOne.setName(d.getPlayerName);
-		g.createField();
-		g.printField(field);
-		g.fillField();
-		g.printField(field);
-		g.shot();
-		g.printField(field);
-	}
+ }
+ }
+ 
+	 
+	 public void shipsAreReady(){
+		 System.out.println("Ships are ready!");
+	 }
+	 
+	 public void weHaveWinner(String name){
+		 System.out.println("The " + name + " is the last sailor swimming!");
+	 }
 }
 
 
@@ -189,21 +104,19 @@ Ship(int decks, int x, int y){
 	this.decks = decks;
 	decksList.add(new Deck(x, y));
 	if (decks>1){
-if (direction.equals('v')){
+//if (direction.equals('v')){
 	for(int i=1; i<decks; i++){
-		deckList.add(new Deck(x, y+i));
+		decksList.add(new Deck(x, y+i));
+	}
+//}
 	}
 }
-	}
-}
-	
-
-
 public int getDecks(){
 	return decks;
 }
 
 }
+
 
 public class Player{
 	String name;
