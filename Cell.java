dda21bf8a9fe0fba7a6cf87630cc00dcdd185132
Sergey <sefimenko13@gmail.com>
2017-03-07 14:32:32 +0300

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -35,21 +35,7 @@ public class Cell{
 		
 	}
 		
-	public void setX(int x){
-	this x=x;
-	}
-		
-		public void setY(int y){
-			this y=y;
-		}
-		
-		public int getX(){
-			return x;
-		}
-		
-		public int getY(){
-		return y;
-		}
+
 	
 	public void shotAt(){
 	
