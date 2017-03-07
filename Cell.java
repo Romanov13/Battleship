670932646b4371b872cf5d7650a96ce71a9a07e9
Sergey <sefimenko13@gmail.com>
2017-03-07 14:46:19 +0300

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -3,16 +3,19 @@ public class Cell{
 
 	String shape;
 	Deck d;
+	boolean shotAt;
 	
 
 	Cell(String type){
 		d = null;
 		 shape = type;
+		shotAt = false;
 	}
 	
 	Cell(){
 		d = null;
 		 shape = "Empty";
+		shotAt = false;
 	}
 
 	public String getShape(){
@@ -38,12 +41,14 @@ public class Cell{
 
 	
 	public void shotAt(){
-	
+	if(shotAt==false){
 	if(d==null){
 	shape = "Miss";	
 	} else {
 	shape = d.hit();
 	}
+		shotAt = true;
+	} else {}
 	}
 
 }
