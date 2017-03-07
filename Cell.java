bb2114a7de5fc24a075a04d959e38dbdd1f51930
Sergey <sefimenko13@gmail.com>
2017-03-07 14:24:09 +0300

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -3,14 +3,20 @@ public class Cell{
 
 	String shape;
 	Deck d;
+	int x;
+	int y;
 
-	Cell(String type){
+	Cell(int x, int y, String type){
 		d = null;
+		this x=x;
+		this y=y;
 		 shape = type;
 	}
 	
-	Cell(){
+	Cell(int x, int y){
 		d = null;
+		this x=x;
+		this y=y;
 		 shape = "Empty";
 	}
 
@@ -33,6 +39,22 @@ public class Cell{
 		} else {shape = "Empty";
 		
 	}
+		
+	public void setX(int x){
+	this x=x;
+	}
+		
+		public void setY(int y){
+			this y=y;
+		}
+		
+		public int getX(){
+			return x;
+		}
+		
+		public int getY(){
+		return y;
+		}
 	
 	public void shotAt(){
 	
