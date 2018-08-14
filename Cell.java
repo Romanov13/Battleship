2f534c1e8a505a2e2867b0af03652f032ed1ebaa
Sergey <sefimenko13@gmail.com>
2018-08-14 19:29:30 +0300

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,29 +1,4 @@
-public class Cell{
+enum Cell{
 
-
-	String shape;
-
-	Cell(String type){
-		changeTo(type);
-	}
-
-	public String getShape(){
-		return shape;
-	}
-
-	public void changeTo(String type){
-		if(type.equals("Deck")){
-			shape = "|p";
-		}
-		if(type.equals("Hit")){
-			shape = "|x";
-		}
-		if(type.equals("Empty")){
-			shape = "| ";
-		}
-		if(type.equals("Miss")){
-			shape = "|*";
-		}
-	}
-
+DECK, MISS, EMPTY, HIT, OUTLINE
 }
